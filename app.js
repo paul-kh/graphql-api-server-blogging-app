85fa433f356fb8a3cda1c16609db06ffbc1353f8
Paul Chheang <phalcheat@gmail.com>
2021-01-20 22:54:33 -0600

    setup connection to MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
+// IMPORTED MODULES
 const express = require("express");
+const mongoose = require("mongoose");
 
+// INIT INSTANCES
 const app = express();
-const PORT = process.env.PORT | 3000;
 
-app.use("/", (req, res, next) => {
-  res.status(200).send("<h1>Welcome to GraphQL API Server!</h1>");
+// GLOBAL VARIABLES
+const PORT = process.env.PORT | 5000;
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.wvahj.mongodb.net/${MONGO_DB_NAME}`;
+
+// CORS SETTINGS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.listen(PORT, () => {
-  console.log("The server is running on PORT: ", PORT);
-});
+// CONNECT TO MONGODB & START SERVER
+mongoose
+  .connect(MONGO_URI)
+  .then((result) => {
+    app.listen(PORT, () => {
+      console.log("DB connection is successfully established!");
+      console.log("The server is running on PORT", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
